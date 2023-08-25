839d810a9a6ddc92411ae3c2815a2473aa2b432b
Jardel Back Kuhnen <jardel.kuhnen@mercadolivre.com>
2023-08-25 19:32:16 -0300

    Done a switch case with chanels to get cep response from apis.

---
+++ cmd/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	brasilAPi := make(chan string)
+	viaCep := make(chan string)
+	cep := "85811050"
+	go doRequest(brasilAPi, cep, "https://brasilapi.com.br/api/cep/v1/%s")
+	go doRequest(viaCep, cep, "https://viacep.com.br/ws/%s/json/")
+
+	select {
+	case resp := <-brasilAPi:
+		println("Response from brasilapi:")
+		println(resp)
+	case resp := <-viaCep:
+		println("Response from viacep: ")
+		println(resp)
+	case <-time.After(1 * time.Minute):
+		println("Timeout")
+	}
+}
+
+func doRequest(result chan<- string, cep, url string) {
+	resp, err := http.Get(fmt.Sprintf(url, cep))
+	if err != nil {
+		log.Print("error getting response from api: " + err.Error())
+		return
+	}
+	defer resp.Body.Close()
+
+	responseBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response:", err)
+		return
+	}
+	result <- string(responseBody)
+}
